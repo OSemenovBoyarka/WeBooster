26ac157d71f686d0951d9681c43ebb6e85297eb6
Alexander Semenov <asemenovboyarka@gmail.com>
2016-03-24 01:00:11 +0200

    added tun2socks

--- app/src/main/java/com/uawebchallenge/webooster/ProxyVpnService.java
+++ app/src/main/java/com/uawebchallenge/webooster/ProxyVpnService.java
@@ -1,5 +1,7 @@
 package com.uawebchallenge.webooster;
 
+import org.torproject.android.vpn.Tun2Socks;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +11,8 @@ import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * @author Alexander Semenov
@@ -30,11 +34,22 @@ public class ProxyVpnService extends VpnService implements Runnable {
 
     private Thread vpnThread;
 
+    /// tun2socks
+    private Process tun2SocksProcess = null;
+
+    //xSocks
+    private static final String VPN_ADDRESS = "26.26.26.1";
+
+    private static final int VPN_MTU = 1500;
+
+    private static final int PROXY_PORT = 8080;
+
+    private ParcelFileDescriptor vpnInterface;
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         // Stop the previous session by interrupting the thread.
         stopProxy();
-
         vpnThread = new Thread(this, "ProxyVpnThread");
         vpnThread.start();
         return START_STICKY;
@@ -56,27 +71,86 @@ public class ProxyVpnService extends VpnService implements Runnable {
         if (vpnThread != null) {
             vpnThread.interrupt();
         }
+        if (vpnInterface != null) {
+            try {
+                Log.d(TAG, "closing interface, destroying VPN interface");
+
+                vpnInterface.close();
+                vpnInterface = null;
+
+            } catch (Exception e) {
+                Log.d(TAG, "error stopping tun2socks", e);
+            } catch (Error e) {
+                Log.d(TAG, "error stopping tun2socks", e);
+            }
+            Tun2Socks.Stop();
+        }
+
+
     }
+
 
     @Override
     public void run() {
+
         //magic goes here :)
-        ParcelFileDescriptor pfd = new Builder()
-                .addAddress("192.168.0.1", 32)
-                .establish();
+//
+//        Thread forwarderThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (!Thread.currentThread().isInterrupted()) {
+//                    try {
+//                        Log.d(TAG, "run: listening for network requests...");
+//                        ServerSocket serverSocket = new ServerSocket(PROXY_PORT);
+//                        Socket socket = serverSocket.accept();
+//                        Log.d(TAG, "run: accepted socket: " + socket);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//            }
+//        });
+//        forwarderThread.start();
         try {
-            while (!Thread.currentThread().isInterrupted()) {
-                Thread.sleep(100);
-                Log.d(TAG, "Listening connections");
+            final String virtualGateway = "10.10.10.1";
+            final String virtualIP = "10.10.10.2";
+            final String virtualNetMask = "255.255.255.0";
+            final String dummyDNS
+                    = "8.8.8.8"; //this is intercepted by the tun2socks library, but we must put in a valid DNS to start
+            final String defaultRoute = "0.0.0.0";
+
+            final String localSocks = "127.0.0.1" + ":" + PROXY_PORT;
+            final String localDns = "127.0.0.1" + ":" + PROXY_PORT;
+
+            Builder builder = new Builder();
+            builder.setSession(getString(R.string.app_name));
+            builder.setMtu(VPN_MTU);
+            builder.addAddress(virtualGateway, 32);
+            builder.addDnsServer(dummyDNS);
+
+            //we will route all traffic
+            builder.addRoute(defaultRoute, 0);
+
+            vpnInterface = builder.establish();
+            if (vpnInterface == null) {
+                Log.e(TAG, "vpn interface is null");
             }
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            Tun2Socks.Start(vpnInterface, VPN_MTU, virtualIP, virtualNetMask, localSocks, localDns,
+                    true);
+
+        } catch (Exception e) {
+            Log.d(TAG, "tun2Socks has stopped", e);
         }
-        try {
-            pfd.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (vpnInterface != null){
+            try {
+                vpnInterface.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            vpnInterface = null;
         }
     }
+
 }
